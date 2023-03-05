0d7c337f0defd19fe127fa54a69195b98bcbc728
Osorabrian <osorabrian@gmail.com>
2023-03-05 21:46:03 +0300

    completed project

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ class ApplicationController < Sinatra::Base
       end
     
       options "*" do
-        response.headers["Allow"] = "GET, PUT, POST, DELETE, OPTIONS"
+        response.headers["Access-Control-Allow-Methods"] = "GET, PUT, PATCH, POST, DELETE, OPTIONS"
         response.headers["Access-Control-Allow-Headers"] = "Authorization, Content-Type, Accept, X-User-Email, X-Auth-Token"
         response.headers["Access-Control-Allow-Origin"] = "*"
         200
@@ -21,42 +21,53 @@ class ApplicationController < Sinatra::Base
         Meme.all.to_json
     end
 
+    get "/mymemes/:id" do
+        User.find(params[:id]).memes.to_json
+    end
+
     get "/memes/:id" do
         Meme.find(params[:id]).to_json
     end
 
-    get '/user/:id' do
-     User.find(params[:id]).to_json
+    get '/user/:email' do
+     User.find_by(email: params[:email]).to_json
     end
 
     post '/user/create' do
+        new_user = JSON.parse(request.body.read)
+
         User.create(
-            first_name: params[:first_name],
-            last_name: params[:last_name],
-            email: params[:email],
-            password: params[:password],
-            confirm_password: params[:confirm_password]
+            first_name: new_user["first_name"],
+            last_name: new_user["last_name"],
+            email: new_user["email"],
+            password: new_user["password"],
+            confirm_password: new_user["confirm_password"]
         ).to_json
     end
 
-    post '/memes/create/:id' do
-        user = User.find(params[:id])
-        Meme.create(
-            title: params[:title],
-            description: params[:description],
-            image_url: params[:image_url],
-            user_id: user
-        ).to_json
+    post '/memes/create' do
+        new_meme = JSON.parse(request.body.read)
+
+        title = new_meme["title"]
+        image_url = new_meme["image_url"]
+        description = new_meme["description"]
+        user_id = new_meme["user_id"]
+        meme = Meme.create(title: title, image_url: image_url, description: description, user_id: user_id)
+
+        meme.to_json
     end
 
     put '/memes/update/:id' do
-        memes = Meme.find(params[:id])
-        memes.update(
-            title: params[:title],
-            description: params[:description],
-            image_url: params[:image_url]
-        )
-        memes.to_json
+        meme = Meme.find(params[:id])
+
+        new_meme = JSON.parse(request.body.read)
+
+        title = new_meme["title"]
+        image_url = new_meme["image_url"]
+        description = new_meme["description"]
+        meme.update(title: title, image_url: image_url, description: description)
+
+        meme.to_json
     end
 
     delete "/memes/delete/:id" do
